3ef03ea956cc1639731e1da5f7875a0db1e15d76
Alec Clews <alec.clews@papercut.com>
2015-02-11 16:43:55 +1100

    Added Printer Script for watermarking

---
+++ PrinterScripts/MakeWaterMarkOptional.js
@@ -0,0 +1,22 @@
+/*
+* Provide the user with opportunity to remove watermark before printing
+*
+* The default watermakr are used unless teh users enters a valid override code
+*/
+
+var OVERRIDE_CODE = "Plain"
+
+function printJobHook(inputs, actions) {
+
+  var response = actions.client.promptForText("Please the enter appropriate code to remove watermarking:",{"defaultText": "Watermark"});
+
+  if (response  == "TIMEOUT" || response == "CANCEL") {
+    // Timed out, so cancel the job and exit script.
+    actions.job.cancel();
+    return;
+  }
+
+  if (response  == OVERRIDE_CODE ) {
+    actions.job.setWatermark("");
+  } // else  use configured defaults
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PaperCutExamples
+
+This repository contains various examples of scripts and other things that PaperCut users may find useful.
+
+PaperCut is a powerful Copier and Printer management system. More details at http://papercut.com/
+
+All the files in this repository come with ABSOLUTELY NO WARRANTY in th hope they will be useful.
+This is free software, and you are welcome to redistribute it under the terms of the The MIT License,
+refer to the file license for more details
+
+Copyright (C) 2015  PaperCut Software Pty Ltd.
