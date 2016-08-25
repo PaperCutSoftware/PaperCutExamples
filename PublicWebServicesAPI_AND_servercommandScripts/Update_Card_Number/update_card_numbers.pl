3e5621f0c628513d8f9e7f23a3501d63b30e9944
Alec Clews <alec.clews@papercut.com>
2016-08-25 14:43:27 +1000

    Various improvments
    1) Simplfy Input handling
    2) Improve response checking
    3) Remove string concatonation operator
    4) use strict and related changes
    5) Add comments
    6) Use better variable names
    7) Change execute bit
    8) Add shbang line
    9) Improve execution reporting. Only print summary of succesful updates.

--- PublicWebServicesAPI_AND_servercommandScripts/Update_Card_Number/update_card_numbers.pl
+++ PublicWebServicesAPI_AND_servercommandScripts/Update_Card_Number/update_card_numbers.pl
@@ -1,32 +1,39 @@
-# Update Card Numbers from CSV
+#!/usr/bin/env perl
+#
+## Update Card Numbers from CSV
 # Uses RPC::XML - http://search.cpan.org/~rjray/RPC-XML-0.79/lib/RPC/XML.pm
-# Usage: update_card_number.pl sample.csv
+# to support https connections also install LWP::Protocol::https
+
+# Usage: resp_number.pl sample.csv, or reads account details from stdin
 
 use RPC::XML;
 use RPC::XML::Client;
 
-$server = "papercut";
-$token = "this_is_my_token";
-$url = "http://" . $server . ":9191/rpc/api/xmlrpc";
+use strict;
 
+my $server = "localhost"; my $port="9191";
+my $token = "password";
+my $url = "http://${server}:${port}/rpc/api/xmlrpc";
 
-my $input = $ARGV[0] or die "No input file was found \n";
-open( my $data, '<', $input ) or die "Failed to open '$input' \n";
+my $client = RPC::XML::Client->new($url);
 
-$client = RPC::XML::Client->new($url);
+my $updated;
 
-while (my $line = <$data>){
-	chomp $line;
-	my @records = split ",", $line;
-	
-	$update_card = $client->send_request( 'api.setUserProperty', $token, $records[0], 'card-number', RPC::XML::string->new($records[1]));
+while (<>){
+  chomp;
+  my ($user, $cardno) = split ",";
 
-	if ($update_card->value == 1){
-		print $records[0] . " has been updated\n";
-	} else {
-		print "Failed to update " . $records[0] . "\n";
-	}
+  my $resp = $client->send_request('api.setUserProperty', $token, $user,
+                'card-number', RPC::XML::string->new($cardno));
+
+  die "$resp\n" unless ref($resp);
+
+  if ($resp->value == 1){
+    $updated++
+  } else {
+    print STDERR "Failed to update $user\n";
+  }
 
 }
+print "\nFinshed: $updated users have been updated\n";
 
-print "Finished \n";
