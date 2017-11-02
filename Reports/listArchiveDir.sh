52e3c3518ef660ac03e5c50c16f118422bcc5ed0
Alec Clews <alec.clews@papercut.com>
2017-11-02 16:37:18 +1100

    New script to discover archive locations

---
+++ Reports/listArchiveDir.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# For Mac or Linux servers running a Postgrsql database
+# Modify to suite
+
+# Displays the location of all print archives for a specific printer,
+# printed after a certain date/time
+
+#  $1  printserver
+#  $2  prinnter name
+#  $3  date of last run
+
+# For example
+#  $ ./getArchiveLocations.sh laptop-serv1 fake_printer "1999-01-08 04:05:06"
+
+# Note -- to discover the correct path on a Windows system
+# take the value from tbl_printer_usage_log.archive_path & replace '/' with '\'
+
+DATABASE=papercutdb
+DBUSER=papercut
+DIRSEP='/'
+
+# Are the archives somewhere other than the default?
+p="$(server-command get-config archiving.path)"
+
+if [[ -z $p ]] ; then
+  p=~papercut/server/data/archive  # Default location
+fi
+
+
+psql -Atqd $DATABASE -U $DBUSER --field-separator=$DIRSEP <<EOF
+\set p '$p'
+\set server '$1'
+\set printer '$2'
+\set lastrun '$3'
+select :'p',tbl_printer_usage_log.archive_path,tbl_printer_usage_log.job_uid
+from tbl_printer_usage_log, tbl_printer
+where tbl_printer_usage_log.usage_date > :'lastrun'
+and tbl_printer_usage_log.archive_path is not null
+and tbl_printer.server_name = :'server'
+and tbl_printer.printer_name = :'printer'
+and tbl_printer_usage_log.printer_id = tbl_printer.printer_id
+EOF
+
