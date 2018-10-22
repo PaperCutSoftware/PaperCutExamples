73ac60a1a1597d7b491e4cb60861bd979ffff641
Alec Clews <alec.clews@papercut.com>
2018-10-23 08:57:20 +1100

    Refactor

--- PublicWebServicesAPI_AND_servercommandScripts/listPrinterStatuses.py
+++ PublicWebServicesAPI_AND_servercommandScripts/listPrinterStatuses.py
@@ -5,83 +5,112 @@ from ssl import create_default_context, Purpose
 import sys
 import requests
 
+# To use tls see http://docs.python-requests.org/en/master/user/advanced/
 host = "http://localhost:9191/"
 
-healthURL = host + "api/health/printers/{}/status"
+
+healthURL = host + "api/health/{}/{}/status"
 webServicesURL = host + "rpc/api/xmlrpc"
 
+# See https://www.papercut.com/support/resources/manuals/ng-mf/common/topics/tools-monitor-system-health-api-authorization-key.html
 healthAuth = {'Authorization':"XrACcfMNEtfxnEpXeawi52mRneEwkXYd"}
+
+# Value defined in advanced config property "auth.webservices.auth-token". Should be random
 auth = "token"
 
 proxy  = xmlrpc.client.ServerProxy( webServicesURL, verbose=False,
       context = create_default_context(Purpose.CLIENT_AUTH))
 
+def getDevicePrinterID(server, printer):
 
-offset = 0
-
-limit = 100 # Max number of printers to retrieve on each call
-
-while True:
     try:
-        printerList = proxy.api.listPrinters(auth, offset,limit)
+        return proxy.api.getPrinterProperty(auth, server, printer, "printer-id" )
     except xmlrpc.client.Fault as error:
-        print("\ncalled listPrinters(). Return fault is {}".format(error.faultString))
+        # If the printer is not found this exception will get thrown
+        print("\ncalled getPrinterProperty(). Return fault is {}".format(error.faultString))
         sys.exit(1)
     except xmlrpc.client.ProtocolError as error:
         print("\nA protocol error occurred\nURL: {}\nHTTP/HTTPS headers: {}\nError code: {}\nError message: {}".format(
             error.url, error.headers, error.errcode, error.errmsg))
         sys.exit(1)
+    
+def getDevicePrinterStatus(server, printer):
 
-    for sp in printerList:
-        if sp == "!!template printer!!":
-            continue
-        splitID = sp.split("\\")
-        server= splitID[0]
-        printer = splitID[1]
-        if server == "device":
-          continue
+    printerID = getDevicePrinterID(server, printer)
+
+    if server == "device":
+        type="devices"
+    else:
+        type="printers"
+
+    try:
+        printerStatusResponse = requests.get( healthURL.format(type, printerID), headers=healthAuth)
+    except ConnectionError as error:
+        print("Network problem calling health API: {}".format(error))
+        sys.exit(1)
+    except requests.exceptions.Timeout as error:
+        print("Network call to  health API timed out: {}".format(error))
+        sys.exit(1)
+    except requests.exceptions.RequestException as error:
+        print("Network error on health API failed: {}".format(error))
+        sys.exit(1)
+
+    try:
+        jsonResponse = printerStatusResponse.json()
+    except ValueError as error:
+        print("Could decode json from health API: {}".format(error))
+        sys.exit(1)
+
+    return printerStatusResponse.status_code, jsonResponse["status"]
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+
+    offset = 0
+
+    limit = 100 # Max number of printers to retrieve on each call
+
+    while True:
         try:
-            printerID = proxy.api.getPrinterProperty(auth, server, printer, "printer-id" )
+            printerList = proxy.api.listPrinters(auth, offset,limit)
         except xmlrpc.client.Fault as error:
-            print("\ncalled getPrinterProperty(). Return fault is {}".format(error.faultString))
+            print("\ncalled listPrinters(). Return fault is {}".format(error.faultString))
             sys.exit(1)
         except xmlrpc.client.ProtocolError as error:
             print("\nA protocol error occurred\nURL: {}\nHTTP/HTTPS headers: {}\nError code: {}\nError message: {}".format(
                 error.url, error.headers, error.errcode, error.errmsg))
             sys.exit(1)
-        
-        try:
-          printerStatusResponse = requests.get( healthURL.format(printerID), headers=healthAuth)
-        except ConnectionError as error:
-          print("Network problem calling health API: {}".format(error))
-          sys.exit(1)
-        except Timeout as error:
-          print("Network call to  health API timed out: {}".format(error))
-          sys.exit(1)
-        except requests.exceptions.RequestException as error:
-          print("Network error on health API failed: {}".format(error))
-          sys.exit(1)
 
-        try:
-          jsonResponse = printerStatusResponse.json()
-        except ValueError as error:
-          print("Could decode json from health API: {}".format(error))
-          sys.exit(1)
+        for sp in printerList:
+            if sp == "!!template printer!!":
+                continue
+            splitID = sp.split("\\")
+            server= splitID[0]
+            printer = splitID[1]
 
+            status, text = getDevicePrinterStatus(server, printer)
 
-        if printerStatusResponse.status_code != 200:
-            print("Status for printer {}/{} is {}  -- PRINTER IN ERROR ".format(
-                  server,
-                  printer,
-                  jsonResponse["status"]))
-        else:
-            print("Status for printer {}/{} is {}".format(
-                  server,
-                  printer,
-                  jsonResponse["status"]))
+            if server == "device":
+                type="device"
+            else:
+                type="printer"
 
-    if len(printerList) < limit:
-        break # We have reached the end
+            if status != 200:
+                print("Status for {} {}/{} is {} -- DEVICE/PRINTER IN ERROR".format(
+                    type,
+                    server,
+                    printer,
+                    text))
+            else:
+                print("Status for {} {}/{} is {}".format(
+                    type,
+                    server,
+                    printer,
+                    text))
 
-    offset += limit # We need to next slice of printers
+        if len(printerList) < limit:
+            break # We have reached the end
+
+        offset += limit # We need to next slice of printers
 
